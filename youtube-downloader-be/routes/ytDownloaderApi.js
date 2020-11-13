3238760ee7e244b302f552509419c1264ee9093c
Glenn Arne Christensen <glennarne.christensen@gmail.com>
2020-11-13 18:21:49 +0100

    WORKS, and WE GET INFO!!! SHIET MAN

--- youtube-downloader-be/routes/ytDownloaderApi.js
+++ youtube-downloader-be/routes/ytDownloaderApi.js
@@ -11,7 +11,7 @@ router.get('/', function (req, res, nextFunc) {
 // For å kjøre denne http request, så må url være "localhost:9000/ytdownloaderApi"
 router.post('/', function (req, res, nextFunc) {
   const url = req.body.url;
-  var videoName = 'what';
+  let videoName = 'what';
 
   console.log('VALUE: ', url);
 
@@ -20,15 +20,27 @@ router.post('/', function (req, res, nextFunc) {
   });
 
   video.on('info', function (info) {
+    var videoInfo = {
+      title: info.title,
+      description: info.description,
+      duration: info._duration_hms,
+      uploader: info.uploader,
+      thumbnail: info.thumbnail,
+      size: info.size,
+    };
+
     console.log('Download started');
-
-    videoName = info._filename; // * Får ikke til å assign navn....
-
-    console.log('filename: ', info._filename);
+    // console.log('INFO: ', info);
+    console.log('filename: ', info.title);
     console.log('size: ', info.size);
+
+    // Send video info (Funket ikke å assigne også gjøre det etter 'video.on' funksjonen, så bare sender inni her)
+    res.send(videoInfo);
   });
 
-  video.pipe(fs.createWriteStream(videoName + '.mp3'));
+  video.pipe(fs.createWriteStream(videoName + '-hhh.mp3'));
+
+  // res.send(`I received your POST request: ${videoName}`);
 
   // TODO: Slett videon etterpå
 
--- youtube-downloader-fe/src/components/YoutubeDownloader/YoutubeDownloader.tsx
+++ youtube-downloader-fe/src/components/YoutubeDownloader/YoutubeDownloader.tsx
@@ -3,24 +3,45 @@ import styled from 'styled-components';
 import axios from 'axios';
 import { YoutubeDownloadForm } from './molecules/YoutubeDownloadForm';
 
+interface VideoInfo {
+  title: string;
+  description: string;
+  duration: string; // 00:00:00
+  uploader: string;
+  thumbnail: string;
+  size: string;
+}
+
 const YoutubeDownloader: React.FC = () => {
   const [ytUrl, setYtUrl] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [videoInfo, setVideoInfo] = React.useState<VideoInfo>();
 
   const downloadFromUrl = () => {
+    setIsLoading(true);
     console.log('Try to send url: ', ytUrl);
 
     // fetch('http://localhost:9000/ytDownloaderApi').then((res) => res.text());
 
     axios
       .post('http://localhost:9000/ytDownloaderApi', { url: ytUrl })
-      .then((res) => console.log('URL SENT, res: ', res))
-      .catch((err) => console.log('ERROR: ', err));
+      .then((res) => {
+        console.log('URL SENT, res: ', res);
+        setVideoInfo(res.data);
+      })
+      .catch((err) => console.log('ERROR: ', err))
+      .finally(() => setIsLoading(false));
   };
 
   return (
     <Container>
-      {ytUrl}
+      {isLoading && <h4 style={{ color: 'slateblue' }}>Loading.....</h4>}
       <YoutubeDownloadForm downloadVideo={downloadFromUrl} setYtUrl={setYtUrl} />
+
+      <h2>{videoInfo?.title}</h2>
+      <p>{videoInfo?.duration}</p>
+      <p>{videoInfo?.uploader}</p>
+      <img src={videoInfo?.thumbnail} alt="video thumbnail" width="200px" />
     </Container>
   );
 };
